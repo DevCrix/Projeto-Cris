50b28a11a690e06ecd596e06cf94adb97bb37f9e
Cristiano Vieira <127705099+DevCrix@users.noreply.github.com>
2023-07-20 19:00:24 -0300

    menu responsivo

--- static/script.js
+++ static/script.js
@@ -6,6 +6,6 @@ const body = document.querySelector("body");
 menuMobile.addEventListener("click", () => {
     menuMobile.classList.contains("bi-list")
         ? menuMobile.classList.replace("bi-list", "bi-x")
-        : menuMobile.ClasslList.replace("bi-x", "bi-list");
+        : menuMobile.classList.replace("bi-x", "bi-list");
     body.classList.toggle("menu-nav-active");
 });
